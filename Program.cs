40b07287b5cc5719e7a5ed85473a90f4fb39d877
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-10-03 16:53:26 +0200

    Updated Valdumerte

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿//int first = 2;
-//string second = "4";
-//string result = first + second;
-//Console.WriteLine(result + "\n");
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result + "\n");
 
 // int myInt = 3;
 // Console.WriteLine($"int: {myInt}");
 // decimal myDecimal = myInt;
 // Console.WriteLine($"decimal: {myDecimal}" + "\n");
 
-//decimal myDecimal = 3.14m;
-//Console.WriteLine($"decimal: {myDecimal}");
-//int myInt = (int)myDecimal;
-//Console.WriteLine($"int: {myInt}" + "\n");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}" + "\n");
 
 decimal myDecimal = 1.23456789m;
 float myFloat = (float)myDecimal;
@@ -23,7 +23,26 @@ int second = 7;
 string message = first.ToString() + second.ToString();
 Console.WriteLine(message + "\n");
 
-int value = (int)1.5m; 
-Console.WriteLine(value);
-int value2 = Convert.ToInt32(1.5m);
-Console.WriteLine(value2);
+string first1 = "5";
+string second1 = "7";
+int sum = int.Parse(first1) + int.Parse(second1);
+Console.WriteLine(sum + "\n");
+
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result + "\n");
+
+int value3 = (int)1.5m; 
+Console.WriteLine(value3);
+int value4 = Convert.ToInt32(1.5m); 
+Console.WriteLine(value4 + "\n");
+
+string value5 = "bad";
+int result1 = 0;
+if (int.TryParse(value, out result)) {
+    Console.WriteLine($"Measurement: {result}");
+} else {
+    Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
--- obj/Debug/net8.0/Valdumerte.AssemblyInfo.cs
+++ obj/Debug/net8.0/Valdumerte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Valdumerte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a909b77389e5a02535ed0654fc446de04465e96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8eb0119c68c344f0729b91c24c97a68505df8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Valdumerte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Valdumerte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
